eb04d291a15dd59b280fed7f07f1472d6aec628c
ojotuk <ojotuk14@yahoo.com>
2021-04-16 18:28:35 +0100

    updated pay affiliate

--- routes/payments.js
+++ routes/payments.js
@@ -1,95 +1,117 @@
-const express = require('express')
-const { requireJWT } = require('../middleware/auth')
-const {todaysDate,addMonth,addYear} = require('./../helper/date/date')
-const router = express.Router()
-const Payments = require('../models/Payments')
-const Profiles = require('../models/Profiles')
-
-
-
+const express = require("express");
+const { requireJWT } = require("../middleware/auth");
+const { todaysDate, addMonth, addYear } = require("./../helper/date/date");
+const router = express.Router();
+const Payments = require("../models/Payments");
+const Profiles = require("../models/Profiles");
 
 //add product
 
-router.post('/app/payment/paystack/marketplace', requireJWT, async (req,res)=>{
-    const {ref,amount,package,cycle} = req.body
+router.post(
+  "/app/payment/paystack/marketplace",
+  requireJWT,
+  async (req, res) => {
+    const { ref, amount, package, cycle } = req.body;
+
     //
-    const {email} = req.user
+    const { email } = req.user;
+    const item = {
+      ref,
+      amount,
+      package,
+      cycle,
+      email: email,
+      service: "marketplace",
+    };
 
-    const profile = await Profiles.findOne({email:email});
+    const profile = await Profiles.findOne({ email: email });
 
-    const handleExpire = ()=>{
-        switch (cycle) {
-            case 'monthly':
-                return addMonth(1)
-            case 'yearly':
-                return addYear(1)
-            default:
-                break;
-        }
+    const handleExpire = () => {
+      switch (cycle) {
+        case "monthly":
+          return addMonth(1);
+        case "yearly":
+          return addYear(1);
+        default:
+          break;
+      }
+    };
+    if (package === "Gold") {
+      profile.subscriptionLevel = 3;
+      profile.subscriptionStart = todaysDate();
+      profile.subscriptionEnd = handleExpire();
+      profile.save();
+      //Store Payment
+      const newPayment = new Payments(item);
+      newPayment.save();
     }
-    if(package==='Gold'){
-        profile.subscriptionLevel=3;
-        profile.subscriptionStart=todaysDate();
-        profile.subscriptionEnd = handleExpire()
-        profile.save()
+    if (package === "Silver") {
+      profile.subscriptionLevel = 2;
+      profile.subscriptionStart = todaysDate();
+      profile.subscriptionEnd = handleExpire();
+      profile.save();
+      //Store Payment
+      const newPayment = new Payments(item);
+      newPayment.save();
     }
-    if(package==='Silver'){
-        profile.subscriptionLevel=2;
-        profile.subscriptionStart=todaysDate();
-        profile.subscriptionEnd = handleExpire()
-        profile.save()
-    }
-    if(package==='Bronze'){
-        profile.subscriptionLevel=1;
-        profile.subscriptionStart=todaysDate();
-        profile.subscriptionEnd = handleExpire()
-        profile.save()
-    }
-    // const storeInfo=profile.storeInfo
-    if (profile.referral.isReffered && profile.referral.count===1){
-        const item = {ref,amount,package,cycle,email:email,service:'marketplace'}
-        const newPayment = new Payments(item)
-        newPayment.save()
-        res.json({code:201, msg:"payment added"})
-
-    } else if(!profile.referral.isReffered && profile.referral.count===0){
-        const item = {ref,amount,package,cycle,email:email,service:'marketplace'}
-        const newPayment = new Payments(item)
-        newPayment.save()
-        res.json({code:201, msg:"payment added"})
-    } else if(profile.referral.isReffered && profile.referral.count===0){
-        const findRef = await Profiles.findOne({referral})
-        if(!findRef.refCode){
-            res.status(404).json({error:"referral code doesn''t exist"})
-        } else {
-              //push the user the referrers profile list
-
-        let newReferrer =    {
-            amount:amount,
-            email:email,
-            package:package,
-            cycle:cycle,
-            commission:0.15*amount
-          }
-        findRef.earnings.push(newReferrer)
-        findRef.markModified('earnings')
-        findRef.save()
-
-        const item = {ref,amount,package,cycle,email:email,service:'marketplace'}
-        const newPayment = new Payments(item)
-        newPayment.save()
-        res.json({code:201, msg:"payment added"})
-        }
+    if (package === "Bronze") {
+      profile.subscriptionLevel = 1;
+      profile.subscriptionStart = todaysDate();
+      profile.subscriptionEnd = handleExpire();
+      profile.save();
+      //Store Payment
+      const newPayment = new Payments(item);
+      newPayment.save();
     }
 
+    //credit affiliate
+    const isRef = profile.referral.isReffered;
+    console.log(isRef);
+    if (!isRef) return res.json({ msg: "added not reffered" });
+    if (isRef && profile.referral.count === 1)
+      return res.json({ msg: "reffered but count is one" });
 
-})
+    if (isRef && profile.referral.count === 0) {
+      const isRefBy = profile.referral.refCode;
+      const affiliateProfile = await Profiles.findOne({
+        affiliateCode: isRefBy,
+      });
+    //   console.log(affiliateProfile);
+      let newCommission = {
+        amount: amount,
+        email: email,
+        package: package,
+        cycle: cycle,
+        commission: 0.15 * Number(amount),
+      };
+      affiliateProfile.earnings.push(newCommission);
+      affiliateProfile.markModified("earnings");
+         //change count to 1
+         await profile.referral.count === 1;
+        //  profile.markModified("referral");
+       const updated= await profile.save();
+       console.log(updated)
+      const saved = await affiliateProfile.save()
+      
+     
+    return res.json({ msg: "added, affiliate credited" });
 
+      
+    }
 
-router.get('/app/payment/paystack/get-marketplace-payments',requireJWT, async (req,res)=>{
-    const {userType}=req.user;
+    //
 
-    const pays = await Payments.find()
-})
+  }
+);
 
-module.exports = router
+router.get(
+  "/app/payment/paystack/get-marketplace-payments",
+  requireJWT,
+  async (req, res) => {
+    const { userType } = req.user;
+
+    const pays = await Payments.find();
+  }
+);
+
+module.exports = router;
