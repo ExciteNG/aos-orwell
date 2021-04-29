6542dc4d0a7529676236ea408be44da9b96460df
ojotuk <ojotuk14@yahoo.com>
2021-04-29 15:30:00 +0100

    added welcome email

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const CheckName = require('../models/Checkname')
 const BusinessReg = require('../models/businessreg')
 const Users = require('./../models/User');
-
+const nodeoutlook = require('nodejs-nodemailer-outlook')
+const WelcomeMail = require("./../emails/new_welcome_templates")
 
 router.post('/confirmation/email', async (req, res) => {
 
@@ -16,6 +17,23 @@ router.post('/confirmation/email', async (req, res) => {
   if(acctToken===token) {
     await Users.findOneAndUpdate({email:email},{emailVerified:true})
     // await account.save()
+    WelcomeMail(account.name)
+
+    nodeoutlook.sendEmail({
+      auth: {
+          user: "enquiry@exciteafrica.com",
+          pass: "ExciteManagement123$"
+      },
+      from: 'enquiry@exciteafrica.com',
+      to: account.email,
+      subject: `Welcome ${account.name}`,
+      html: WelcomeMail(account.name),
+      text:WelcomeMail(account.name),
+      replyTo: 'enquiry@exciteafrica.com',
+      onError: (e) => console.log(e),
+      onSuccess: (i) => console.log(i),
+      secure:false
+  });
     return res.json({code:201,message:"account confirm",email:email}
   )}else{
     res.json({code:400,message:"Invalid"})
