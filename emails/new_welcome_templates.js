5790084ed48f6a9045fc24912c88c4cdba0e3e29
Osagie <osagieiyayi09@gmail.com>
2021-04-30 17:04:12 +0100

    added email host field

--- emails/new_welcome_templates.js
+++ emails/new_welcome_templates.js
@@ -1,3 +1,4 @@
+const emailHost = require('./email_host')
 const welcomeMail = (userName) =>{
 
     return `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
