b9a834d0fe547a8664c8e222e7b43a3f1a7f4fed
Osagie <osagieiyayi09@gmail.com>
2021-05-03 21:31:04 +0100

    added further templating

--- routes/dealroutes.js
+++ routes/dealroutes.js
@@ -86,7 +86,7 @@ router.put('/deal/:id',upload, async (req,res) =>{
     }
 })
 
-//delete a story from the database
+//delete a deal from the database
 router.delete('/delete/:id', async (req,res) => {
 
     const id = req.params.id
