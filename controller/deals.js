4dd0af0963f82f45d048ac40372013663009fc58
ojotuk <ojotuk14@yahoo.com>
2021-05-04 08:42:44 +0100

    added deals controller

---
+++ controller/deals.js
@@ -0,0 +1,45 @@
+const Deals = require("./../models/deals");
+const Profiles = require('./../models/Profiles')
+// const Deals = require('../models/deals');
+
+const addDeals = async (req, res) => {
+    const {email,userType}= req.user;
+    // console.log(email)
+    const userProfile = await Profiles.findOne({email:email})
+    const userStore = userProfile.storeInfo
+  const {
+    category,
+    dealImg,
+    title,
+    prevPrice,
+    dealPrice,
+    desc,
+    endDate,
+    subCategory,
+  } = req.body;
+  const percent = (prevPrice-dealPrice)*100/prevPrice
+  const addDeals = new Deals({
+    category: category,
+    dealImg: dealImg,
+    title: title,
+    prevPrice: prevPrice,
+    dealPrice:dealPrice,
+    storeInfo:userStore,
+    subCategory:subCategory,
+    endDate,endDate,
+    desc:desc,
+    email:email,
+    discnt:`-${percent}%`
+    })
+  try {
+    await addDeals.save();
+    return res.status(201).json({ code:201,message: "success" });
+  } catch (err) {
+    console.error(err);
+    res.send({ status: 500, message: err.message });
+  }
+};
+
+module.exports = {
+  addDeals,
+};
