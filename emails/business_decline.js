d8a68a74a04383d8a1def2948295f2a077ed62f3
Osagie <osagieiyayi09@gmail.com>
2021-05-05 11:08:12 +0100

    modified emails social links

--- emails/business_decline.js
+++ emails/business_decline.js
@@ -888,18 +888,19 @@ const businessDecline = () => {
                                                                                         </td>
                                                                                     </tr>
                                                                                     <tr>
-                                                                                        <td class="esd-block-social es-p10t es-p20b" align="center" style="font-size: 0px; background-color: #6fd20d;" bgcolor="#6fd20d">
-                                                                                            <table class="es-table-not-adapt es-social" cellspacing="0" cellpadding="0">
-                                                                                                <tbody>
-                                                                                                    <tr>
-                                                                                                        <td class="es-p10r" valign="top" align="center"><a target="_blank" href><img title="Twitter" src="https://stripo.email/cabinet/assets/editor/assets/img/social-icons/square-gray/twitter-square-gray.png" alt="Tw" width="24" height="24"></a></td>
-                                                                                                        <td class="es-p10r" valign="top" align="center"><a target="_blank" href><img title="Facebook" src="https://stripo.email/cabinet/assets/editor/assets/img/social-icons/square-gray/facebook-square-gray.png" alt="Fb" width="24" height="24"></a></td>
-                                                                                                        <td class="es-p10r" valign="top" align="center"><a target="_blank" href><img title="Instagram" src="https://stripo.email/cabinet/assets/editor/assets/img/social-icons/square-gray/instagram-square-gray.png" alt="Ig" width="24" height="24"></a></td>
-                                                                                                        <td valign="top" align="center"><a target="_blank" href><img title="Google+" src="https://stripo.email/cabinet/assets/editor/assets/img/social-icons/square-gray/google-plus-square-gray.png" alt="G+" width="24" height="24"></a></td>
-                                                                                                    </tr>
-                                                                                                </tbody>
-                                                                                            </table>
-                                                                                        </td>
+                                                                                    <td class="esd-block-social es-p10t es-p20b" align="center" style="font-size: 0px; background-color: #6fd20d;" bgcolor="#6fd20d">
+                                                                                    <table class="es-table-not-adapt es-social" cellspacing="0" cellpadding="0">
+                                                                                        <tbody>
+                                                                                            <tr>
+                                                                                                <td class="es-p10r" valign="top" align="center"><a target="_blank" href="https://twitter.com/ExciteBusiness"><img title="Twitter" src="https://stripo.email/cabinet/assets/editor/assets/img/social-icons/square-gray/twitter-square-gray.png" alt="Tw" width="24" height="24" style></a></td>
+                                                                                                <td class="es-p10r" valign="top" align="center"><a target="_blank" href="https://www.facebook.com/Excite-Enterprise-109200980956538https://www.facebook.com/Excite-Enterprise-109200980956538"><img title="Facebook" src="https://stripo.email/cabinet/assets/editor/assets/img/social-icons/square-gray/facebook-square-gray.png" alt="Fb" width="24" height="24" style></a></td>
+                                                                                                <td class="es-p10r" valign="top" align="center"><a target="_blank" href="https://www.instagram.com/exciteenterprise/"><img title="Instagram" src="https://stripo.email/cabinet/assets/editor/assets/img/social-icons/square-gray/instagram-square-gray.png" alt="Ig" width="24" height="24" style></a></td>
+                                                                                                <td class="es-p10r" valign="top" align="center"><a target="_blank" href="https://www.linkedin.com/in/excite-africa-4632a71b9/"><img title="LinkedIn" src="https://stripo.email/cabinet/assets/editor/assets/img/social-icons/square-gray/linkedin-square-gray.png" alt="Ig" width="24" height="24"></a></td>
+                                                                                               
+                                                                                            </tr>
+                                                                                        </tbody>
+                                                                                    </table>
+                                                                                </td>
                                                                                     </tr>
                                                                                     <tr>
                                                                                         <td class="esd-block-text" align="center" bgcolor="#6fd20d">
