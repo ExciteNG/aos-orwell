9d2aff3401eb56beb44200ecb3b66bf222110549
ojotuk <ojotuk14@yahoo.com>
2021-05-11 09:32:24 +0100

    made adjustment to partner approval

--- routes/excite/partners.js
+++ routes/excite/partners.js
@@ -7,8 +7,8 @@ const Controller = require('../../controller/excite/partners')
 //  All partners route
 router.get('/all/partners', Controller.getAllPartners);
 
-// Tax partner routes
-router.put('/tax/:id', Controller.updatePartner);
+// excite- partner routes
+router.put('/:id', Controller.updatePartner);
 router.get('/tax/partners', Controller.getAllTaxPartners);
 // Business Partner routes
 router.get('/business/partners', Controller.getAllBusinessPartners);
