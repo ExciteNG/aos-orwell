080ff7dc9b2897f7e7e9515a1dd90e8087c58a2b
ojotuk <ojotuk14@yahoo.com>
2021-05-11 09:04:03 +0100

    update

--- controller/partners/loans/loan.js
+++ controller/partners/loans/loan.js
@@ -2,17 +2,13 @@ const Loan = require('./../../../models/loan')
 
 
 const getAccountAppChannelle = async (req, res) => {
-  const { email } = req.user;
+//   const { email } = req.user;
   //  console.log(email)
-  const profile = await Loan.findOne({ fundingPartner: "CHANNELLE" });
-  const user = {
-    name: profile.fullname.split(" ")[0],
-    partnerCode: profile.partnerCode,
-    regStatus: profile.regStatus.isApproved,
-    accountDetails: profile.accountDetails,
-  };
-
-  res.json({ user });
+  const profile = await Loan.find({ fundingPartner: "CHANNELLE" });
+  if(profile){
+      return res.json({acct:profile})
+  }
+  return res.json({acct:null})
 };
 
 
