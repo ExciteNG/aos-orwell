8991c78ea4c61f723693928ecbcc23aed8d3bd57
ojotuk <ojotuk14@yahoo.com>
2021-05-24 11:21:20 +0100

    upgrade

---
+++ controller/kiosk.js
@@ -0,0 +1,26 @@
+const KioskModel = require('../models/kiosk');
+
+const createKiosk =async (req, res) => {
+  const {email, userType}= req.user;
+// console.log(req.body)
+  const data={...req.body, email}
+
+  const Kiosk = new KioskModel({...data });
+  Kiosk.save()
+    .then((record) => {
+      res.status(201).json({
+        message: 'Application submitted successfully'
+      });
+    })
+    .catch((err) => {console.log(err)
+      res.status(400).json({
+        message: 'Oops! Something went wrong.',
+        err
+      });
+    });
+};
+
+
+module.exports = {
+  createKiosk
+}
