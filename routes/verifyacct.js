1430901fa5fede3cf269b33f587b07ea4e4f03ae
Osagie <osagieiyayi09@gmail.com>
2021-08-24 19:45:31 +0100

    .

--- routes/verifyacct.js
+++ routes/verifyacct.js
@@ -8,30 +8,32 @@ const verifyEmail = require("../emails/verify_template");
 
 router.post('/verify-account', async (req,res) => {
     //filter users who haven't their emails
-    const {email} = req.user;
-    const User = await Users.findOne({email:email}).lean()
-    if (!User) return res.json({code:400,message:"an error occurred !, try again"})
-
-    //send mail
-    nodeoutlook.sendEmail({
-        auth: {
-          user: process.env.EXCITE_ENQUIRY_USER,
-          pass: process.env.EXCITE_ENQUIRY_PASS,
-        },
-        from: "enquiry@exciteafrica.com",
-        to: User.email,
-        subject: "Verify Your Account",
-        html: verifyEmail(User.username, User.email, User.verifyToken),
-        text: verifyEmail(User.username, User.email, User.verifyToken),
-        replyTo: "enquiry@exciteafrica.com",
-        onError: (e) => console.error(e),
-        onSuccess: (i) => console.log(i),
-        secure: false,
-      });
-      return res.json({code:200,message:"a verification mail has been sent to proceed with the next steps"})
-
-
-
+    try {
+        const {email} = req.user;
+        const User = await Users.findOne({email:email}).lean()
+        if (!User) return res.json({code:400,message:"an error occurred !, try again"})
+    
+        //send mail
+        nodeoutlook.sendEmail({
+            auth: {
+              user: process.env.EXCITE_ENQUIRY_USER,
+              pass: process.env.EXCITE_ENQUIRY_PASS,
+            },
+            from: "enquiry@exciteafrica.com",
+            to: User.email,
+            subject: "Verify Your Account",
+            html: verifyEmail(User.username, User.email, User.verifyToken),
+            text: verifyEmail(User.username, User.email, User.verifyToken),
+            replyTo: "enquiry@exciteafrica.com",
+            onError: (e) => console.error(e),
+            onSuccess: (i) => console.log(i),
+            secure: false,
+          });
+          return res.json({code:200,message:"a verification mail has been sent to proceed with the next steps"})
+    } catch(err){
+        console.error(err)
+        return res.json({code:400,message:err.message})
+    }
 
 })
 
